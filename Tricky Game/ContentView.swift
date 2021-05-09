4ef094721f7a2e9af3ed5d1fcb3bf4ff20982103
Dieter Bergmann <dieterbergmann@arcor.de>
2021-05-09 07:40:55 +0200

    Initial Commit

---
+++ Tricky Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tricky Game
+//
+//  Created by Dieter Bergmann on 09.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tricky Game/Tricky_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tricky_GameApp.swift
+//  Tricky Game
+//
+//  Created by Dieter Bergmann on 09.05.21.
+//
+
+import SwiftUI
+
+@main
+struct Tricky_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
